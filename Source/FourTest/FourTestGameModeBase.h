c210b287794be0047c77d4aec44c04e954e4e8af
KimHanSeung <kor_hans@naver.com>
2025-10-08 11:48:02 +0900

    Initialize Project

---
+++ Source/FourTest/FourTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FourTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOURTEST_API AFourTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
